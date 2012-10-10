1fc5d8dfc8d97f611af10f5ba2b1f4cdb419e281
Stanley Hon <stanleyhon348@gmail.com>
2012-10-10 13:47:00 +1100

    Finished parsing command-line arguements.

--- src/dv_routing.java
+++ src/dv_routing.java
@@ -4,5 +4,36 @@
  * CSE Username: shon348
  * UNSW StudentID: z3373433
  */
+
 public class dv_routing {
+
+    public static char NODE_ID;
+    public static int NODE_PORT;
+    public static String CONFIG_FILE_ADDRESS;
+
+    public static void main (String args[]) {
+        if (args.length != 3) {
+            System.out.println("Number of arguments is wrong");
+        }
+        // args[0] is NODE_ID
+        if (args[0].length() == 1 && Character.isLetter(args[0].charAt(0))) {
+            NODE_ID = args[0].charAt(0);
+        } else {
+            System.out.println("NODE_ID is wrong.");
+            return;
+        }
+
+        // args[1] is NODE_PORT
+        try {
+            int NODE_PORT = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println ("NODE_PORT is not numerical!");
+            return;
+        }
+
+        // args[2] is CONFIG.TXT
+        CONFIG_FILE_ADDRESS = args[2];
+
+        return;
+    }
 }
